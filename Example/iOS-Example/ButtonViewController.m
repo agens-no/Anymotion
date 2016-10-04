9fa9dc2eaa5efa9096f917c445acc6568630da9c
Mats Hauge <mats@agens.no>
2016-10-04 16:28:21 +0200

    Button Example updated

--- Example/iOS-Example/ButtonViewController.m
+++ Example/iOS-Example/ButtonViewController.m
@@ -40,57 +40,54 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    CGRect frame = CGRectMake(self.view.center.x - 50.0, self.view.center.y - 10.0, 100.0, 20.0);
-    self.frame = frame;
+    UIColor *selectedColor = [UIColor colorWithRed:93.0/255.0 green:215.0/255.0 blue:9.0/255.0 alpha:1.0];
+    UIColor *unSelectedColor = [UIColor whiteColor];
     
-    UIView *line1 = [[UIView alloc] initWithFrame:self.frame];
-    line1.backgroundColor = [UIColor blueColor];
-    line1.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(45.0));
+    UIView *circleView1 = [self createCircleViewWithSize:CGSizeMake(100.0, 100.0) center:self.view.center fillColor:selectedColor];
+    [self.view addSubview:circleView1];
+    
+    UIView *circleView2 = [self createCircleViewWithSize:CGSizeMake(100.0, 100.0) center:self.view.center fillColor:unSelectedColor];
+    circleView2.layer.transform = CATransform3DMakeScale(0.001, 0.001, 1.0);
+    circleView2.alpha = 0.0;
+    [self.view addSubview:circleView2];
+    
+    UIView *line1 = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 17.0, 7.0)];
+    line1.backgroundColor = [UIColor whiteColor];
+    line1.layer.anchorPoint = CGPointMake(1.0, 0.5);
+    line1.center = CGPointMake(self.view.center.x - 3.5, self.view.center.y + 10.0);
+    line1.layer.transform = CATransform3DMakeRotation(DEGREES_TO_RADIANS(45.0), 0, 0, 1);
     [self.view addSubview:line1];
     
-    UIView *line2 = [[UIView alloc] initWithFrame:self.frame];
-    line2.backgroundColor = [UIColor redColor];
-    line2.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(-45.0));
+    UIView *line2 = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 35.0, 7.0)];
+    line2.backgroundColor = [UIColor whiteColor];
+    line2.layer.anchorPoint = CGPointMake(0.0, 0.5);
+    line2.center = CGPointMake(self.view.center.x - 3.5, self.view.center.y + 10.0);
+    line2.layer.transform = CATransform3DRotate(line2.layer.transform, DEGREES_TO_RADIANS(-45.0), 0, 0, 1);
+    line2.layer.transform = CATransform3DTranslate(line2.layer.transform, -3.5, 0.0, 0.0);
     [self.view addSubview:line2];
     
     ANYAnimation *selectedAnimationGroup = [ANYAnimation createAnimation:^ANYActivity *(ANYSubscriber *subscriber) {
         
-        ANYAnimation *rotation = [ANYUIView animationWithDuration:0.5 block:^{
-            line1.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(-60.0));
-            line2.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(30.0));
-        }];
+        ANYAnimation *anim1 = [[[ANYPOPSpring propertyNamed:kPOPLayerScaleXY] toValueWithSize:CGSizeMake(8.0, 8.0)] animationFor:circleView1.layer];
+        ANYAnimation *anim2 = [[[ANYPOPSpring propertyNamed:kPOPLayerScaleXY] toValueWithSize:CGSizeMake(2.0, 2.0)] animationFor:circleView2.layer];
+        ANYAnimation *anim3 = [[[ANYPOPBasic propertyNamed:kPOPViewBackgroundColor] toValue:selectedColor] animationFor:line1];
+        ANYAnimation *anim4 = [[[ANYPOPBasic propertyNamed:kPOPViewBackgroundColor] toValue:selectedColor] animationFor:line2];
+        ANYAnimation *anim5 = [[[ANYPOPBasic propertyNamed:kPOPViewAlpha] toValue:@1.0] animationFor:circleView2];
         
-        ANYAnimation *frame = [ANYUIView animationWithDuration:0.5 block:^{
-            
-            CGRect frame1 = line1.frame;
-            frame1.size = CGSizeMake(frame1.size.width - 50.0, frame1.size.height);
-            line1.frame = frame1;
-            
-            CGRect frame2 = line2.frame;
-            frame2.origin = CGPointMake(self.view.center.x + 11.0, self.view.center.y + 15.0);
-            frame2.size = CGSizeMake(frame2.size.width - 56.0, frame2.size.height - 20.0);
-            line2.frame = frame2;
-            
-        }];
-        
-        return [[ANYAnimation group:@[rotation, frame]] start];
+        return [[ANYAnimation group:@[anim1, anim2, anim3, anim4, anim5]] start];
         
     }];
     self.selectedAnimationGroup = selectedAnimationGroup;
     
     ANYAnimation *unSelectedAnimationGroup = [ANYAnimation createAnimation:^ANYActivity *(ANYSubscriber *subscriber) {
         
-        ANYAnimation *rotation = [ANYUIView animationWithDuration:0.5 block:^{
-            line1.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(45.0));
-            line2.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(-45.0));
-        }];
+        ANYAnimation *anim1 = [[[ANYPOPSpring propertyNamed:kPOPLayerScaleXY] toValueWithSize:CGSizeMake(1.0, 1.0)] animationFor:circleView1.layer];
+        ANYAnimation *anim2 = [[[ANYPOPSpring propertyNamed:kPOPLayerScaleXY] toValueWithSize:CGSizeMake(0.001, 0.001)] animationFor:circleView2.layer];
+        ANYAnimation *anim3 = [[[ANYPOPBasic propertyNamed:kPOPViewBackgroundColor] toValue:unSelectedColor] animationFor:line1];
+        ANYAnimation *anim4 = [[[ANYPOPBasic propertyNamed:kPOPViewBackgroundColor] toValue:unSelectedColor] animationFor:line2];
+        ANYAnimation *anim5 = [[[ANYPOPBasic propertyNamed:kPOPViewAlpha] toValue:@0.0] animationFor:circleView2];
         
-        ANYAnimation *frame = [ANYUIView animationWithDuration:0.5 block:^{
-            line1.frame = self.frame;
-            line2.frame = self.frame;
-        }];
-        
-        return [[ANYAnimation group:@[frame]] start];
+        return [[ANYAnimation group:@[anim1, anim2, anim3, anim4, anim5]] start];
         
     }];
     self.unSelectedAnimationGroup = unSelectedAnimationGroup;
@@ -98,6 +95,29 @@
     UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
     [self.view addGestureRecognizer:gesture];
 }
+
+
+
+#pragma mark - Create
+
+- (UIView *)createCircleViewWithSize:(CGSize)size center:(CGPoint)center fillColor:(UIColor *)fillColor
+{
+    CGRect frame = (CGRect){CGPointZero, size};
+    
+    CAShapeLayer *circleLayer = [CAShapeLayer layer];
+    circleLayer.path = [UIBezierPath bezierPathWithOvalInRect:frame].CGPath;
+    circleLayer.fillColor = fillColor.CGColor;
+    
+    UIView *circleView = [[UIView alloc] initWithFrame:frame];
+    circleView.center = center;
+    [circleView.layer addSublayer:circleLayer];
+    
+    return circleView;
+}
+
+
+
+#pragma mark - Gesture
 
 - (void)handleGesture:(UIGestureRecognizer *)gesture
 {
