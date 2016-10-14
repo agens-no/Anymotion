4c25d0040932edf834ae2d84cbb09b96897abb42
Håvard Fossli <hfossli@gmail.com>
2016-10-14 12:31:55 +0200

    Refactor list view

--- Example/iOS-Example/ListViewController.m
+++ Example/iOS-Example/ListViewController.m
@@ -25,8 +25,9 @@
 
 #import <Anymotion/Anymotion.h>
 
+
 @interface ListViewController ()
-@property (nonatomic, strong) NSArray *views;
+@property (nonatomic, strong) NSArray <UIView *> *views;
 @property (nonatomic, strong) UIView *containerView;
 @end
 
@@ -46,23 +47,89 @@
 {
     [super viewDidLoad];
     
+    UIGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView:)];
+    [self.view addGestureRecognizer:tapRecognizer];
+    
     UIView *containerView = [[UIView alloc] initWithFrame:self.view.bounds];
     containerView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:containerView];
     self.containerView = containerView;
     
-    NSArray *views = [self listOfViews];
-    for (UIView *view in views)
-    {
-        [self.containerView addSubview:view];
-    }
-    self.views = views;
-    
-    ANYAnimation *animations = [self showListAnimationWithViews:views];
-    [[animations delay:1.0] start];
+    [[[self showList] delay:0.5] start];
 }
 
-- (NSArray *)listOfViews
+- (void)didTapView:(UIGestureRecognizer *)gesture
+{
+    ANYAnimation *hideAndShow = [[self hideList] then:[[self showList] delay:1.0]];
+    [hideAndShow start];
+}
+
+- (ANYAnimation *)showList
+{
+    UIView *container = self.containerView;
+    
+    return [ANYAnimation defer:^ANYAnimation *{
+                
+        NSArray <UIView *> *views = [self createList];
+        for (UIView *view in views)
+        {
+            [container addSubview:view];
+        }
+        self.views = views;
+        
+        ANYAnimation *groupAnimation = [ANYAnimation empty];
+        NSTimeInterval delay = 0.0;
+        
+        for (UIView *view in views)
+        {
+            NSValue *toValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
+            CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.19 : 1.0 : 0.22 : 1.0];
+            
+            ANYAnimation *animation = [[[[[[[[ANYCABasic keyPath:@"transform"] toValue:toValue] timingFunction:timingFunction] updateModel] animationFor:view.layer] before:^{
+                view.layer.anchorPoint = CGPointMake(0.0, 0.5);
+            }] after:^{
+                CGPoint center = view.center;
+                center.x = self.containerView.center.x;
+                view.layer.anchorPoint = CGPointMake(0.5, 0.5);
+                view.center = center;
+            }] delay:delay];
+            
+            groupAnimation = [groupAnimation groupWith:animation];
+            
+            delay += 0.1;
+        }
+        
+        return groupAnimation;
+        
+    }];
+}
+
+- (ANYAnimation *)hideList
+{
+    NSArray <UIView *> *views = self.views;
+    CGRect frame = self.view.bounds;
+    frame.origin.y = frame.size.height / 2.0;
+    
+    UIColor *backgroundColor = views.firstObject.backgroundColor;
+    UIView *newContainer = [[UIView alloc] initWithFrame:frame];
+    newContainer.backgroundColor = backgroundColor;
+    newContainer.layer.transform = CATransform3DMakeScale(1.0, 0.0, 1.0);
+    newContainer.layer.anchorPoint = CGPointMake(0.5, 1.0);
+    [self.view addSubview:newContainer];
+    
+    UIView *oldContainer = self.containerView;
+    self.containerView = newContainer;
+    
+    return [[ANYUIView animationWithDuration:0.4 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionAllowUserInteraction block:^{
+        newContainer.layer.transform = CATransform3DIdentity;
+        oldContainer.transform = CGAffineTransformMakeScale(0.8, 0.8);
+    }] after:^{
+        [oldContainer removeFromSuperview];
+        newContainer.transform = CGAffineTransformIdentity;
+    }];
+}
+
+- (NSArray <UIView *> *)createList
 {
     NSUInteger numberOfItems = 3;
     CGFloat padding = 100.0;
@@ -70,7 +137,7 @@
     CGFloat height = (self.containerView.frame.size.height - self.navigationController.navigationBar.frame.size.height - padding) / numberOfItems;
     CGRect frame = CGRectMake((padding / 2.0) - (width / 2.0), self.navigationController.navigationBar.frame.size.height + padding / 2.0, width, height);
     
-    NSMutableArray *views = [NSMutableArray array];
+    NSMutableArray <UIView *> *views = [NSMutableArray array];
     CATransform3D transform = CATransform3DMakeScale(0.0, 1.0, 1.0);
     CGFloat hue = [self randomNumberBetween:0.0 maxNumber:255.0];
     CGFloat saturation = 255.0;
@@ -90,79 +157,11 @@
     return views;
 }
 
-- (ANYAnimation *)showListAnimationWithViews:(NSArray *)views
-{
-    NSMutableArray *animations = [NSMutableArray array];
-    NSTimeInterval delay = 0.0;
-    
-    for (UIView *view in views)
-    {
-        NSValue *toValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
-        CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.19 : 1.0 : 0.22 : 1.0];
-     
-        ANYAnimation *animation = [[[[[[[[ANYCABasic keyPath:@"transform"] toValue:toValue] timingFunction:timingFunction] updateModel] animationFor:view.layer] before:^{
-            view.layer.anchorPoint = CGPointMake(0.0, 0.5);
-        }] after:^{
-            CGPoint center = view.center;
-            center.x = self.containerView.center.x;
-            view.layer.anchorPoint = CGPointMake(0.5, 0.5);
-            view.center = center;
-        }] delay:delay];
-        
-        [animations addObject:animation];
-        
-        delay += 0.1;
-    }
-    
-    return [ANYAnimation group:animations];
-}
-
 - (UIView *)viewWithFrame:(CGRect)frame color:(UIColor *)color
 {
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView:)];
     UIView *view = [[UIView alloc] initWithFrame:frame];
-    [view addGestureRecognizer:tapGesture];
     view.backgroundColor = color;
     return view;
-}
-
-- (void)didTapView:(UIGestureRecognizer *)gesture
-{
-    CGRect frame = self.view.bounds;
-    frame.origin.y = frame.size.height / 2.0;
-    
-    UIColor *backgroundColor = gesture.view.backgroundColor;
-    UIView *view = [[UIView alloc] initWithFrame:frame];
-    view.backgroundColor = backgroundColor;
-    view.layer.transform = CATransform3DMakeScale(1.0, 0.0, 1.0);
-    view.layer.anchorPoint = CGPointMake(0.5, 1.0);
-    [self.view addSubview:view];
-    
-    [[[ANYUIView animationWithDuration:0.4 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut block:^{
-        view.layer.transform = CATransform3DIdentity;
-        self.containerView.transform = CGAffineTransformMakeScale(0.8, 0.8);
-    }] onCompletion:^{
-        
-        for (UIView *v in self.views)
-        {
-            [v removeFromSuperview];
-        }
-        
-        self.containerView.transform = CGAffineTransformIdentity;
-        self.containerView.backgroundColor = backgroundColor;
-        [view removeFromSuperview];
-        
-        NSArray *views = [self listOfViews];
-        for (UIView *view in views)
-        {
-            [self.containerView addSubview:view];
-        }
-        self.views = views;
-        
-        ANYAnimation *animations = [self showListAnimationWithViews:self.views];
-        [[animations delay:2.0] start];
-        
-    }] start];
 }
 
 - (NSInteger)randomNumberBetween:(NSInteger)min maxNumber:(NSInteger)max
